653650e42b3a14767381af67fe9f3c3e7f83cccd
Pieter Joost van de Sande <pj@born2code.net>
2014-03-26 10:44:26 +0100

    Switch to static reply

--- main.go
+++ main.go
@@ -8,13 +8,16 @@ import (
 
 var (
 	host = flag.String("host", "localhost:8080", "the host address to bind to")
+
+	helloWorldString = "Hello, World!"
+	helloWorldBytes  = []byte(helloWorldString)
 )
 
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
 		rw.Header().Set("Content-Type", "text/plain")
-		rw.Write([]byte(req.RequestURI))
+		rw.Write(helloWorldBytes)
 	})
 
 	fmt.Println("hosting echo service at %v", *host)
